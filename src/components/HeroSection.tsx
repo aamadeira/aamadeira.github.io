8c0e8e53c635fd216b244f207a57ac07ce41d4c0
alexmadeira23 <alexandrealemaomadeira@gmail.com>
2025-06-20 10:44:25 +0100

    title update

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -36,7 +36,7 @@ export default function HeroSection() {
                         </motion.h1>
 
                         <motion.p className="text-xl text-muted-foreground mb-6" variants={childVariants}>
-                            Software Engineer 👨‍💻
+                            Software Developer 👨‍💻
                         </motion.p>
 
                         <motion.div className="flex flex-col gap-2 items-center md:items-start" variants={containerVariants}>
